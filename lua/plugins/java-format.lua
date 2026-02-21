8495f5395663eefbae23017afe5d0c79ed117423
najeeb-alshabibi-dojo <najeeb.al-shabibi@dojo.tech>
2026-02-21 18:19:21 +0000

    initial commit

---
+++ lua/plugins/java-format.lua
@@ -0,0 +1,18 @@
+-- Configure google-java-format for Java formatting
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        java = { "google-java-format" },
+      },
+      formatters = {
+        ["google-java-format"] = {
+          command = "google-java-format",
+          args = { "-" },
+          stdin = true,
+        },
+      },
+    },
+  },
+}
