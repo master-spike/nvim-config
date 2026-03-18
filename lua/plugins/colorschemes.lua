6c8dda0afd63ede2501cf643bc8a34c77dd105d0
najeeb-alshabibi-dojo <najeeb.al-shabibi@dojo.tech>
2026-03-18 21:43:00 +0000

    gruvbox color scheme with inlay hint and gitsigns overrides

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,9 +1,21 @@
 return {
   { "EdenEast/nightfox.nvim" },
   {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      overrides = {
+        LspInlayHint = { bg = "#504945" },
+        GitSignsAdd = { fg = "#76CD30" },
+        GitSignsAddLn = { fg = "#76CD30" },
+        GitSignsChange = { fg = "#64B0C3" },
+        GitSignsChangeLn = { fg = "#64B0C3" },
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "carbonfox",
+      colorscheme = "gruvbox",
     },
   },
 }
