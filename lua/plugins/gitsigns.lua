d120d4a5d5a0883fb51827109dbb1af83254c73b
najeeb-alshabibi-dojo <najeeb.al-shabibi@dojo.tech>
2026-06-11 23:41:28 +0100

    rebind ]h and [h to hunk navigation

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -9,21 +9,24 @@ require("gitsigns").setup({
     end
 
     -- Navigation
-    map("n", "]c", function()
+    local function nav_next_hunk()
       if vim.wo.diff then
-        vim.cmd.normal({ "]c", bang = true })
+        vim.cmd.normal({ "]h", bang = true })
       else
         gitsigns.nav_hunk("next")
       end
-    end)
+    end
 
-    map("n", "[c", function()
+    local function nav_prev_hunk()
       if vim.wo.diff then
-        vim.cmd.normal({ "[c", bang = true })
+        vim.cmd.normal({ "[h", bang = true })
       else
         gitsigns.nav_hunk("prev")
       end
-    end)
+    end
+
+    map("n", "]h", nav_next_hunk, { desc = "Next hunk" })
+    map("n", "[h", nav_prev_hunk, { desc = "Prev hunk" })
 
     -- Actions
     map("n", "<leader>ghs", gitsigns.stage_hunk)
