c45febad96f7b7963b4ef29b94c95d51088fe19c
master-spike <najeeb.al.shabibi@gmail.com>
2026-06-13 23:02:16 +0100

    support for inner function C

---
+++ after/queries/c/textobjects.scm
@@ -0,0 +1,9 @@
+; extends
+; Add function.inner for function_definition (C doesn't define it by default)
+; Captures the function body statements, allowing dif/yif/etc to work
+
+(function_definition
+  body: (compound_statement
+    "{"
+    (_)+ @function.inner
+    "}"))
